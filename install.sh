ed64a3b3714aade3dac28ada7241dce370be9c3a
Gray Leonard <gl7039a@american.edu>
2015-01-06 20:22:46 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 wget https://github.com/grayleonard/termwallet/raw/master/termwallet.zip -O /tmp/termwallet.zip
+mkdir /usr/local/bin/termwallet-repo/
 unzip -o /tmp/termwallet.zip -d /usr/local/bin/
 rm /tmp/termwallet.zip
