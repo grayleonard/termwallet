a25968c15cb4f13b139e2a828bcaaa393ca99a5f
Gray Leonard <gl7039a@american.edu>
2015-01-17 21:36:27 -0500

    Fixed Restore descriptions & password

--- src/main/java/com/bitcoin/termwallet/Commands/CommandRestore.java
+++ src/main/java/com/bitcoin/termwallet/Commands/CommandRestore.java
@@ -6,12 +6,11 @@ import org.bitcoinj.core.Address;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
-@Parameters(commandDescription = "Remove a key from your watched addresses - destroy imported private keys")
+@Parameters(commandDescription = "Create a wallet based on a BIP-39 mnemonic seed")
 public class CommandRestore {
 
 	@Parameter(names = { "-t", "--time" }, description = "Replay blockchain starting from this time (UNIX epoch time)")
 	long time = System.currentTimeMillis() / 1000L;
-	@Parameter(names = { "-p", "--password" }, description = "Seed password (True or false)")
-	boolean passRequired;
+	
 }
 
